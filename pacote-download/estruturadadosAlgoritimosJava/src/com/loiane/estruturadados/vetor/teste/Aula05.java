49ca32d92d259a5452a690016df76017f3faf75e
Mihay Victor <111930015+mihayvictor@users.noreply.github.com>
2023-12-26 20:14:05 -0300

    Ignorar Aula05

--- pacote-download/estruturadadosAlgoritimosJava/src/com/loiane/estruturadados/vetor/teste/Aula05.java
+++ pacote-download/estruturadadosAlgoritimosJava/src/com/loiane/estruturadados/vetor/teste/Aula05.java
@@ -4,18 +4,20 @@ import com.loiane.estruturadados.vetor.Vetor;
 
 public class Aula05 {
 
-	public static void main(String[] args) throws IllegalAccessException {
+	/*CLASSE IGNORADA
+	 
+	  public static void main(String[] args) throws IllegalAccessException {
 		Vetor vetor = new Vetor(10);
 		
 		vetor.adiciona("elemento1");
 		vetor.adiciona("elemento2");
 		vetor.adiciona("elemento3");
 		
-		System.out.println(vetor.busca(3));
+		System.out.println(vetor.busca(3));*
 		
 		
 		
 
-	}
+	}*/
 
 }
